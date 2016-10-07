038652e7cad33cdb6f93a936ac6ee9c8a738cfca
Tarik Merzouk <tarikmerzouk@yahoo.com>
2016-10-07 15:53:32 -0700

    Update hello_app.py

--- hello_app.py
+++ hello_app.py
@@ -8,15 +8,13 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def hello():
-    return get_random_thing()
-
-def get_random_thing() :
     adjs = adjs = urllib2.urlopen("https://raw.githubusercontent.com/bellcodo/potential-octo-chainsaw/master/adjectives.lst").read()
     adjs = adjs.split()
     
     random_selection = random.choice(adjs)
-    
     return random_selection
+
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
