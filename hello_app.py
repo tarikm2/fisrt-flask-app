747aab091bde7cb49b81a61b164d0056c51dc46a
Tarik Merzouk <tarikmerzouk@yahoo.com>
2016-10-07 15:41:29 -0700

    Update hello_app.py

--- hello_app.py
+++ hello_app.py
@@ -1,12 +1,22 @@
 import os
 from flask import Flask
+import random
+import urllib2
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route("/")
 def hello():
-    return "Hello world!"
+    return get_random_thing()
+
+def get_random_thing() :
+    adjs = adjs = urllib2.urlopen("https://raw.githubusercontent.com/bellcodo/potential-octo-chainsaw/master/adjectives.lst").read()
+    adjs = adjs.split()
+    
+    random_selection = random.choice(adjs)
+    
+    return random_selection
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
